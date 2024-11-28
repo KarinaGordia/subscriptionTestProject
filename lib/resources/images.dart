d1a2074080dda4331006533cb12debb44494691e
Karina Gordia <kgordya24@gmail.com>
2024-11-28 14:33:37 +0100

    first setup
    assets, dependencies

---
+++ lib/resources/images.dart
@@ -0,0 +1,8 @@
+part of 'resources.dart';
+
+class Images {
+  Images._();
+
+  static const String ellipseBottom = 'assets/images/ellipseBottom.png';
+  static const String ellipseTop = 'assets/images/ellipseTop.png';
+}
