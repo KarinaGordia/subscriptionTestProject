35b5e78a662f38a69712d7d6b3a1a00501a69f3b
Karina Gordia <kgordya24@gmail.com>
2024-11-29 12:43:14 +0100

    gradient button refactor

--- lib/ui/widgets/gradient_button.dart
+++ lib/ui/widgets/gradient_button.dart
@@ -3,11 +3,13 @@ import 'package:subscription_test/ui/theme/theme.dart';
 
 class GradientButton extends StatelessWidget {
   const GradientButton({
-    super.key, required this.text, required this.onPressed,
+    super.key, required this.text, required this.onPressed, this.size,
   });
 
   final String text;
   final void Function()? onPressed;
+  final WidgetStateProperty<Size?>? size;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,18 +23,16 @@ class GradientButton extends StatelessWidget {
         ),
       ),
       child: FilledButton(
-        style: const ButtonStyle(
-          shape: WidgetStatePropertyAll(
+        style: ButtonStyle(
+          shape: const WidgetStatePropertyAll(
             RoundedRectangleBorder(
               borderRadius: BorderRadius.all(
                 Radius.circular(29.0),
               ),
             ),
           ),
-          minimumSize: WidgetStatePropertyAll(
-            Size(double.infinity, 60),
-          ),
-          backgroundColor: WidgetStatePropertyAll(Colors.transparent),
+          minimumSize: size,
+          backgroundColor: const WidgetStatePropertyAll(Colors.transparent),
         ),
         onPressed: onPressed,
         child: Text(
